89de2baf09f5fae7818b641efb4d813120432ed7
denissejoyce <denisse.alido@gmail.com>
2025-02-10 12:14:03 -0600

    desktop view ready 🚀

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 'use strict';
 
 const ratings = document.getElementsByClassName('btn__rating');
+
+for (let ind = 0; ind < ratings.length; ind++) {
+  ratings[ind].addEventListener('mouseover', function () {
+    ratings[ind].classList.add('hover');
+  });
+  ratings[ind].addEventListener('mouseout', function () {
+    ratings[ind].classList.remove('hover');
+    ratings[ind].classList.remove('base');
+
+    ratings[ind].classList.add('after');
+    setTimeout(() => {
+      ratings[ind].classList.remove('after');
+    }, 500);
+  });
+}
+
+function getRating() {}
